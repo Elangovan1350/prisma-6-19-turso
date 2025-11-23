7ca510d98be24e04baf43cd561377db75789af5f
Elangovan1350 <elangovan2019miss@gmail.com>
2025-11-23 08:30:09 +0530

    change2

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,8 @@ import { PrismaLibSql } from "@prisma/adapter-libsql";
 const app = new Hono();
 
 const adapter = new PrismaLibSql({
-  url: `${process.env.TURSO_DATABASE_URL}`,
-  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+  url: process.env.TURSO_DATABASE_URL || "",
+  authToken: process.env.TURSO_AUTH_TOKEN || "",
 });
 const prisma = new PrismaClient({ adapter });
 
